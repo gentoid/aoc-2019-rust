b9afb51450a62918031d45b17e24095ebfcff35a
Viktor Lazarev <taurus101v@gmail.com>
2019-12-04 20:22:30 +0100

    Solve 01 / 02

--- aoc-01/src/main.rs
+++ aoc-01/src/main.rs
@@ -5,14 +5,22 @@ use std::{
 
 fn main() {
     let nums = File::open("aoc-01/input-01.txt").and_then(read_and_parse);
-    match nums.map(aoc_01_01) {
-        Ok(res) => println!("{}", res),
+    match nums.as_ref().map(aoc_01_01) {
+        Ok(res) => println!("01 / 01: {}", res),
+        Err(err) => println!("{}", err),
+    }
+    match nums.as_ref().map(aoc_01_02) {
+        Ok(res) => println!("01 / 02: {}", res),
         Err(err) => println!("{}", err),
     }
 }
 
-fn aoc_01_01(nums: Vec<i32>) -> i32 {
+fn aoc_01_01(nums: &Vec<i32>) -> i32 {
     nums.iter().map(calculate_fuel).sum()
+}
+
+fn aoc_01_02(nums: &Vec<i32>) -> i32 {
+    nums.iter().map(calculate_total_fuel).sum()
 }
 
 fn read_and_parse(file: File) -> io::Result<Vec<i32>> {
@@ -34,4 +42,21 @@ fn calculate_fuel(mass: &i32) -> i32 {
     } else {
         fuel
     }
+}
+
+fn calculate_total_fuel(mass: &i32) -> i32 {
+    let mut total_fuel = 0;
+    let mut mass = *mass;
+
+    loop {
+        let fuel = calculate_fuel(&mass);
+        if fuel == 0 {
+            break
+        }
+
+        total_fuel += fuel;
+        mass = fuel;
+    }
+
+    total_fuel
 }
