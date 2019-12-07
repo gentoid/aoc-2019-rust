13572ceb546485a0094f8a81249cf4c96e3db692
Viktor Lazarev <taurus101v@gmail.com>
2019-12-07 10:30:45 +0100

    Complete 03 / 01 with 0 compile errors

--- src/aoc_03.rs
+++ src/aoc_03.rs
@@ -50,17 +50,17 @@ fn parse_movement(movement: &str) -> Diff {
     }
 }
 
-fn calcNextPoint(from: &Point, diff: &Diff) -> Point {
+fn calc_next_point(from: &Point, diff: &Diff) -> Point {
     Point {
         x: from.x + diff.x,
         y: from.y + diff.y,
     }
 }
 
-fn newSegment(from: Point, diff: &Diff) -> Segment {
+fn new_segment(from: Point, diff: &Diff) -> Segment {
     Segment {
-        p1: from,
-        p2: calcNextPoint(&from, diff),
+        p1: from.clone(),
+        p2: calc_next_point(&from, diff),
         direction: if diff.x == 0 {
             Direction::Vertical
         } else {
@@ -69,15 +69,68 @@ fn newSegment(from: Point, diff: &Diff) -> Segment {
     }
 }
 
-fn toSegments(diffs: &Vec<Diff>) -> Vec<Segment> {
-    let point = Point { x: 0, y: 0 };
-    let segments = vec![];
+fn to_segments(diffs: &Vec<Diff>) -> Vec<Segment> {
+    let mut point = Point { x: 0, y: 0 };
+    let mut segments = vec![];
 
     for diff in diffs.iter() {
-        let segment = newSegment(point.clone(), diff);
+        let segment = new_segment(point, diff);
+        point = segment.p2.clone();
         segments.push(segment);
-        point = segment.p2;
     }
 
     segments
+}
+
+fn is_horizontal(segment: &Segment) -> bool {
+    use Direction::*;
+
+    match &segment.direction {
+        Horizontal => true,
+        Vertical => false,
+    }
+}
+
+fn in_range(what: &i32, start: &i32, end: &i32) -> bool {
+    if start < end {
+        start < what && what < end
+    } else {
+        end < what && what < start
+    }
+}
+
+fn find_intersection_one_way(segm1: &Segment, segm2: &Segment) -> Option<Point> {
+    if is_horizontal(segm1)
+        && !is_horizontal(segm2)
+        && in_range(&segm1.p1.y, &segm2.p1.y, &segm2.p2.y)
+        && in_range(&segm2.p1.x, &segm1.p1.x, &segm1.p2.x)
+    {
+        Some(Point {
+            x: segm2.p1.x,
+            y: segm1.p1.y,
+        })
+    } else {
+        None
+    }
+}
+
+fn find_intersection(segm1: &Segment, segm2: &Segment) -> Option<Point> {
+    find_intersection_one_way(segm1, segm2).or(find_intersection_one_way(segm1, segm2))
+}
+
+fn manhattan_distance(point: &Point) -> i32 {
+    point.x.abs() + point.y.abs()
+}
+
+pub fn aoc_03_01() -> i32 {
+    let data = read_and_parse();
+    let mut intersections: Vec<Point> = vec![];
+
+    for segm1 in to_segments(&data[0]) {
+        for segm2 in to_segments(&data[1]) {
+            find_intersection(&segm1, &segm2).map(|point| intersections.push(point));
+        }
+    }
+
+    intersections.iter().map(manhattan_distance).min().unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod aoc_01;
 mod aoc_02;
+mod aoc_03;
 
 fn main() {
     println!("01 / 01: {}", aoc_01::aoc_01_01());
     println!("01 / 02: {}", aoc_01::aoc_01_02());
     println!("02 / 01: {}", aoc_02::aoc_02_01());
     println!("02 / 02: {}", aoc_02::aoc_02_02());
+    println!("03 / 01: {}", aoc_03::aoc_03_01());
 }
