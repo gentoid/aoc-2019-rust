e85b7357f3179ac3d304fd4ec1969274b7794b19
Viktor Lazarev <taurus101v@gmail.com>
2019-12-08 15:59:51 +0100

    Rename 'address" to 'instruction_pointer"

--- src/opcode_computer.rs
+++ src/opcode_computer.rs
@@ -1,6 +1,6 @@
 pub struct Program {
     memory: Vec<usize>,
-    address: usize,
+    instruction_pointer: usize,
     halted: bool,
 }
 
@@ -8,7 +8,7 @@ impl Program {
     pub fn new(memory: Vec<usize>) -> Self {
         Self {
             memory,
-            address: 0,
+            instruction_pointer: 0,
             halted: false,
         }
     }
@@ -22,24 +22,24 @@ impl Program {
     }
 
     fn tick(&mut self) {
-        let opcode = self.memory[self.address];
+        let opcode = self.memory[self.instruction_pointer];
 
         match opcode {
             1 => self.opcode_with_3_args(|a, b| a + b),
             2 => self.opcode_with_3_args(|a, b| a * b),
             99 => {
                 self.halted = true;
-                self.address += 1;
+                self.instruction_pointer += 1;
             }
             _ => unreachable!("Wrong OpCode {}!", opcode),
         }
     }
 
     fn opcode_with_3_args(&mut self, f: fn(usize, usize) -> usize) {
-        let arg1 = self.memory[self.memory[self.address + 1]];
-        let arg2 = self.memory[self.memory[self.address + 2]];
-        let put_to = self.memory[self.address + 3].clone();
+        let arg1 = self.memory[self.memory[self.instruction_pointer + 1]];
+        let arg2 = self.memory[self.memory[self.instruction_pointer + 2]];
+        let put_to = self.memory[self.instruction_pointer + 3].clone();
         self.memory[put_to] = f(arg1, arg2);
-        self.address += 4;
+        self.instruction_pointer += 4;
     }
 }
