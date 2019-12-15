efef1d808f76511b09abd405de185dbf981bf4dc
Viktor Lazarev <taurus101v@gmail.com>
2019-12-15 21:31:00 +0100

    Add functions to generate all the available combinations

--- src/aoc_07.rs
+++ src/aoc_07.rs
@@ -7,6 +7,89 @@ pub fn aoc_07_02() -> u32 {
     0
 }
 
+fn swap_4(seq: &Vec<u32>) -> Vec<Vec<u32>> {
+    let mut seqs = vec![];
+    
+    let mut new_seqs = swap_3(seq);
+    let seq = new_seqs.last().unwrap().clone();
+    seqs.append(&mut new_seqs);
+
+    let seq = swap(&seq, 0, 4);
+    seqs.push(seq.clone());
+    let mut new_seqs = swap_3(&seq);
+    let seq = new_seqs.last().unwrap().clone();
+    seqs.append(&mut new_seqs);
+
+    let seq = swap(&seq, 0, 4);
+    seqs.push(seq.clone());
+    let mut new_seqs = swap_3(&seq);
+    let seq = new_seqs.last().unwrap().clone();
+    seqs.append(&mut new_seqs);
+
+    let seq = swap(&seq, 0, 4);
+    seqs.push(seq.clone());
+    let mut new_seqs = swap_3(&seq);
+    let seq = new_seqs.last().unwrap().clone();
+    seqs.append(&mut new_seqs);
+
+    let seq = swap(&seq, 0, 4);
+    seqs.push(seq.clone());
+    let mut new_seqs = swap_3(&seq);
+    seqs.append(&mut new_seqs);
+    
+    seqs
+}
+
+fn swap_3(seq: &Vec<u32>) -> Vec<Vec<u32>> {
+    let mut seqs = vec![];
+
+    let mut new_seqs = swap_2(seq);
+    let seq = new_seqs.last().unwrap().clone();
+    seqs.append(&mut new_seqs);
+
+    let seq = swap(&seq, 1, 3);
+    seqs.push(seq.clone());
+    let mut new_seqs = swap_2(&seq);
+    let seq = new_seqs.last().unwrap().clone();
+    seqs.append(&mut new_seqs);
+    
+    let seq = swap(&seq, 1, 3);
+    seqs.push(seq.clone());
+    let mut new_seqs = swap_2(&seq);
+    let seq = new_seqs.last().unwrap().clone();
+    seqs.append(&mut new_seqs);
+    
+    let seq = swap(&seq, 0, 3);
+    seqs.push(seq.clone());
+    let mut new_seqs = swap_2(&seq);
+    seqs.append(&mut new_seqs);
+
+    seqs
+}
+
+fn swap_2(seq: &Vec<u32>) -> Vec<Vec<u32>> {
+    let mut seqs = vec![];
+    let seq = swap(seq, 0, 1);
+    seqs.push(seq.clone());
+    let seq = swap(&seq, 1, 2);
+    seqs.push(seq.clone());
+    let seq = swap(&seq, 0, 1);
+    seqs.push(seq.clone());
+    let seq = swap(&seq, 1, 2);
+    seqs.push(seq.clone());
+    let seq = swap(&seq, 0, 1);
+    seqs.push(seq);
+    seqs
+}
+
+fn swap(seq: &Vec<u32>, i: usize, j: usize) -> Vec<u32> {
+    let mut seq = seq.clone();
+    let tmp = seq[i];
+    seq[i] = seq[j];
+    seq[j] = tmp;
+    seq
+}
+
 fn amplifier(phase_settings: &Vec<u32>, program: &Vec<isize>) -> isize {
     let mut input = 0;
     for phase in phase_settings {
