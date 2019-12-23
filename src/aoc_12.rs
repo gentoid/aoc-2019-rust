ac264e96941bc7e7b02fde545ce7fa0f090640e1
Viktor Lazarev <taurus101v@gmail.com>
2019-12-23 20:17:05 +0100

    Add Moon1D struct

--- src/aoc_12.rs
+++ src/aoc_12.rs
@@ -28,7 +28,7 @@ fn prepare_input() -> Vec<Moon> {
         .iter()
         .map(|l| {
             let line = parse_line(l.as_ref());
-            Moon::new(line[0], line[1], line[2])
+            Moon::new((line[0], line[1], line[2]), (0, 0, 0))
         })
         .collect()
 }
@@ -50,44 +50,66 @@ pub fn aoc_12_01() -> isize {
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 struct Moon {
-    coord: [isize; 3],
-    velocity: [isize; 3],
+    x: Moon1D,
+    y: Moon1D,
+    z: Moon1D,
 }
 
 impl Moon {
-    fn new(x: isize, y: isize, z: isize) -> Self {
+    fn new((x, y, z): (isize, isize, isize), (vx, vy, vz): (isize, isize, isize)) -> Self {
         Self {
-            coord: [x, y, z],
-            velocity: [0, 0, 0],
+            x: Moon1D::new(x, vx),
+            y: Moon1D::new(y, vy),
+            z: Moon1D::new(z, vz),
         }
     }
+
     fn energy(&self) -> isize {
-        let mut potential = 0;
-        let mut kinetic = 0;
+        let (x_pot, x_kin) = self.x.energy();
+        let (y_pot, y_kin) = self.y.energy();
+        let (z_pot, z_kin) = self.z.energy();
 
-        for axis in 0..3 {
-            potential += self.coord[axis].abs();
-            kinetic += self.velocity[axis].abs();
-        }
-
-        potential * kinetic
+        (x_pot + y_pot + z_pot) * (x_kin + y_kin + z_kin)
     }
 
     fn update_velocity(&mut self, other: &Moon) {
-        for axis in 0..3 {
-            if self.coord[axis] > other.coord[axis] {
-                self.velocity[axis] -= 1;
-            }
-            if self.coord[axis] < other.coord[axis] {
-                self.velocity[axis] += 1;
-            }
+        self.x.update_velocity(other.x);
+        self.y.update_velocity(other.y);
+        self.z.update_velocity(other.z);
+    }
+
+    fn update_position(&mut self) {
+        self.x.update_position();
+        self.y.update_position();
+        self.z.update_position();
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Moon1D {
+    position: isize,
+    velocity: isize,
+}
+
+impl Moon1D {
+    fn new(position: isize, velocity: isize) -> Self {
+        Self { position, velocity }
+    }
+
+    fn update_velocity(&mut self, other: Moon1D) {
+        if self.position > other.position {
+            self.velocity -= 1;
+        } else if self.position < other.position {
+            self.velocity += 1;
         }
     }
 
     fn update_position(&mut self) {
-        for axis in 0..3 {
-            self.coord[axis] += self.velocity[axis];
-        }
+        self.position += self.velocity;
+    }
+
+    fn energy(&self) -> (isize, isize) {
+        (self.position.abs(), self.velocity.abs())
     }
 }
 
@@ -117,106 +139,58 @@ mod tests {
 
     #[test]
     fn calculates_velocity_for_pair() {
-        let mut moon1 = Moon {
-            coord: [7, 3, -5],
-            velocity: [-4, 2, 7],
-        };
-        let mut moon2 = Moon {
-            coord: [4, 3, 7],
-            velocity: [-1, -5, 6],
-        };
+        let mut moon1 = Moon::new((7, 3, -5), (-4, 2, 7));
+        let mut moon2 = Moon::new((4, 3, 7), (-1, -5, 6));
 
         update_velocities(&mut vec![&mut moon1, &mut moon2]);
 
         let expected_result = (
-            Moon {
-                coord: [7, 3, -5],
-                velocity: [-5, 2, 8],
-            },
-            Moon {
-                coord: [4, 3, 7],
-                velocity: [0, -5, 5],
-            },
+            Moon::new((7, 3, -5), (-5, 2, 8)),
+            Moon::new((4, 3, 7), (0, -5, 5)),
         );
         assert_eq!(expected_result, (moon1, moon2));
     }
 
     #[test]
     fn updates_position() {
-        let mut moon = Moon {
-            coord: [7, 3, -5],
-            velocity: [-4, 2, 7],
-        };
+        let mut moon = Moon::new((7, 3, -5), (-4, 2, 7));
 
         update_positions(&mut vec![&mut moon]);
 
-        let expected = Moon {
-            coord: [3, 5, 2],
-            velocity: [-4, 2, 7],
-        };
+        let expected = Moon::new((3, 5, 2), (-4, 2, 7));
         assert_eq!(expected, moon);
     }
 
     #[test]
     fn calculates_velocity() {
-        let mut moon1 = Moon {
-            coord: [1, 2, 3],
-            velocity: [5, -2, 0],
-        };
-        let mut moon2 = Moon {
-            coord: [-8, 5, 0],
-            velocity: [6, -8, 4],
-        };
-        let mut moon3 = Moon {
-            coord: [0, 6, -4],
-            velocity: [-5, -6, -2],
-        };
+        let mut moon1 = Moon::new((1, 2, 3), (5, -2, 0));
+        let mut moon2 = Moon::new((-8, 5, 0), (6, -8, 4));
+        let mut moon3 = Moon::new((0, 6, -4), (-5, -6, -2));
 
         update_velocities(&mut vec![&mut moon1, &mut moon2, &mut moon3]);
 
-        let expect1 = Moon {
-            coord: [1, 2, 3],
-            velocity: [3, 0, -2],
-        };
-        let expect2 = Moon {
-            coord: [-8, 5, 0],
-            velocity: [8, -8, 4],
-        };
-        let expect3 = Moon {
-            coord: [0, 6, -4],
-            velocity: [-5, -8, 0],
-        };
+        let expect1 = Moon::new((1, 2, 3), (3, 0, -2));
+        let expect2 = Moon::new((-8, 5, 0), (8, -8, 4));
+        let expect3 = Moon::new((0, 6, -4), (-5, -8, 0));
         assert_eq!((expect1, expect2, expect3), (moon1, moon2, moon3));
     }
 
     #[test]
     fn first_example() {
-        let mut moon1 = Moon::new(-1, 0, 2);
-        let mut moon2 = Moon::new(2, -10, -7);
-        let mut moon3 = Moon::new(4, -8, 8);
-        let mut moon4 = Moon::new(3, 5, -1);
+        let mut moon1 = Moon::new((-1, 0, 2), (0, 0, 0));
+        let mut moon2 = Moon::new((2, -10, -7), (0, 0, 0));
+        let mut moon3 = Moon::new((4, -8, 8), (0, 0, 0));
+        let mut moon4 = Moon::new((3, 5, -1), (0, 0, 0));
 
         let mut moons = vec![&mut moon1, &mut moon2, &mut moon3, &mut moon4];
 
         update_velocities(&mut moons);
         update_positions(&mut moons);
 
-        let mut expect1 = Moon {
-            coord: [2, -1, 1],
-            velocity: [3, -1, -1],
-        };
-        let mut expect2 = Moon {
-            coord: [3, -7, -4],
-            velocity: [1, 3, 3],
-        };
-        let mut expect3 = Moon {
-            coord: [1, -7, 5],
-            velocity: [-3, 1, -3],
-        };
-        let mut expect4 = Moon {
-            coord: [2, 2, 0],
-            velocity: [-1, -3, 1],
-        };
+        let mut expect1 = Moon::new((2, -1, 1), (3, -1, -1));
+        let mut expect2 = Moon::new((3, -7, -4), (1, 3, 3));
+        let mut expect3 = Moon::new((1, -7, 5), (-3, 1, -3));
+        let mut expect4 = Moon::new((2, 2, 0), (-1, -3, 1));
 
         let expect_moons = vec![&mut expect1, &mut expect2, &mut expect3, &mut expect4];
 
@@ -234,22 +208,10 @@ mod tests {
         update_velocities(&mut moons);
         update_positions(&mut moons);
 
-        let mut expect1 = Moon {
-            coord: [5, -3, -1],
-            velocity: [3, -2, -2],
-        };
-        let mut expect2 = Moon {
-            coord: [1, -2, 2],
-            velocity: [-2, 5, 6],
-        };
-        let mut expect3 = Moon {
-            coord: [1, -4, -1],
-            velocity: [0, 3, -6],
-        };
-        let mut expect4 = Moon {
-            coord: [1, -4, 2],
-            velocity: [-1, -6, 2],
-        };
+        let mut expect1 = Moon::new((5, -3, -1), (3, -2, -2));
+        let mut expect2 = Moon::new((1, -2, 2), (-2, 5, 6));
+        let mut expect3 = Moon::new((1, -4, -1), (0, 3, -6));
+        let mut expect4 = Moon::new((1, -4, 2), (-1, -6, 2));
 
         let expect_moons = vec![&mut expect1, &mut expect2, &mut expect3, &mut expect4];
 
